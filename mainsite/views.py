80c553d65797b0d0673a93c7146e7fb1f3ce5b2d
stars-xixi <2623194244@qq.comm>
2018-08-12 19:38:27 +0800

    'transferred'

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.template.loader import get_template
+from datetime import datetime
+
+from django.shortcuts import redirect
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+
+def homepage(request):
+	
+	template = get_template('index.html')
+	now=datetime.now()
+
+	posts = Post.objects.all()
+	html=template.render(locals())
+	
+	return HttpResponse(html)
+	
+def showpost(request, slug):
+	template = get_template('post.html')
+	try:
+		post = Post.objects.get(slug=slug)
+		if post != None:
+			html = template.render(locals())
+			return HttpResponse(html)
+	except:
+		return redirect('/')
